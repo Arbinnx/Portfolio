d2acaa1b105770bd62074d418f7555ac0aeaa1e9
Arbinnx <zzekoamet@gmail.com>
2026-04-23 17:46:27 +0200

    This is a bug fixing commit

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ form.addEventListener("submit", function (e) {
   const name = document.getElementById("name").value.trim();
   const email = document.getElementById("email").value.trim();
   const message = document.getElementById("message").value.trim();
-  const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,}$/;
 
   if (!name || !email || !message) {
     alert("Please fill in all fields.");
